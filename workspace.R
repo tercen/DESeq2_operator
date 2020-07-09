e152c2a8dd8a6b2e062ccae726b851b348d7ceb4
Filipe Cadete <filipe@cadete.eu>
2020-07-09 22:43:33 +0000

    Receives data from Tercen and prepares count matrix.

--- workspace.R
+++ workspace.R
@@ -1,11 +1,14 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "0c98153037f24682e45289f1760072cb")
+options("tercen.stepId"     = "8bf22ba4-978e-4f33-b26c-34ae548f1737")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
+
+(ctx = tercenCtx())  %>% 
+  select(.x, .y, .ci, .ri, js0.condition)
 
 (ctx = tercenCtx())  %>% 
   select(.y, .ci, .ri) %>% 
