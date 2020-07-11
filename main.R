e61c2802fdccaf181f95701fc12024157bb9644c
Filipe Cadete <filipe@cadete.eu>
2020-07-11 15:33:30 +0000

    First draft of operator.

--- main.R
+++ main.R
@@ -1,9 +1,33 @@
 library(tercen)
-library(dplyr)
+library(tidyverse)
+library(DESeq2)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
-  ctx$addNamespace() %>%
+all_data <- (ctx = tercenCtx())  %>% 
+  select(.x, .y, .ci, .ri, js0.condition)
+
+count_matrix <- all_data %>%
+  select(.ri, .x, .y) %>%
+  pivot_wider(names_from = ".x", values_from = ".y")
+
+row_indexes <- count_matrix %>%
+  select(.ri)
+
+count_matrix <- count_matrix %>%
+  select(-.ri)
+
+colData <- select(all_data,
+                  .x, condition = js0.condition) %>%
+  unique()
+
+dds <- DESeqDataSetFromMatrix(countData = count_matrix,
+                              colData = colData,
+                              design = ~ condition)
+
+dds <- DESeq(dds)
+
+dds_results <- results(dds)
+
+dds_results <- tibble(row_indexes, as_tibble(dds_results))
+
+ctx$addNamespace(dds_results) %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -13,7 +13,12 @@ all_data <- (ctx = tercenCtx())  %>%
 
 count_matrix <- all_data %>%
   select(.ri, .x, .y) %>%
-  pivot_wider(names_from = ".x", values_from = ".y") %>%
+  pivot_wider(names_from = ".x", values_from = ".y")
+
+row_indexes <- count_matrix %>%
+  select(.ri)
+
+count_matrix <- count_matrix %>%
   select(-.ri)
 
 colData <- select(all_data,
@@ -27,3 +32,8 @@ dds <- DESeqDataSetFromMatrix(countData = count_matrix,
 dds <- DESeq(dds)
 
 dds_results <- results(dds)
+
+dds_results <- tibble(row_indexes, as_tibble(dds_results))
+
+ctx$addNamespace(dds_results) %>%
+  ctx$save()
