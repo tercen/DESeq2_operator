454a6591256afc6ef7b55c3f2cdb8126d780cb15
lucas501 <lucas.lelann@gmail.com>
2022-08-22 14:24:24 +0000

    update main

--- main.R
+++ main.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(DESeq2)
 library(ashr)
 library(apeglm)
+library(reshape2)
 
 ctx = tercenCtx()
 
@@ -15,12 +16,15 @@ alpha <- as.double(ctx$op.value('alpha'))
 LFC_shrinkage <- as.logical(ctx$op.value('LFC_shrinkage'))
 shrinkage_type <- as.character(ctx$op.value('shrinkage_type'))
 
-all_data <- ctx %>% select(.ci, .ri,.colorLevels)
 
-count_matrix <- ctx$as.matrix()
+all_data <- ctx %>% select(.ci, .ri,.colorLevels, .y)
+
+count_matrix <- acast(all_data, .ri~.ci, value.var=".y")
 
 colData <- filter(all_data, .ri == 0) %>%
   select(.ci, condition = .colorLevels)
+
+colData$condition<-factor(colData$condition)
 
 dds <- DESeqDataSetFromMatrix(countData = count_matrix,
                               colData = colData,
@@ -34,9 +38,12 @@ if(LFC_shrinkage) dds_results <- lfcShrink(dds, type=shrinkage_type,
                                            res = dds_results,
                                            coef = "condition")
 
-dds_results <- tibble(filter(all_data, .ci == 0) %>% select(.ri),
+res_out_tmp <- tibble(filter(all_data, .ci == 0) %>% select(.ri),
                       as_tibble(dds_results) %>%
                         mutate(minus_log10_padj = -log10(padj)))
 
-ctx$addNamespace(dds_results) %>%
+res_out<-res_out_tmp %>%
+  ctx$addNamespace()
+
+res_out %>%
   ctx$save()
