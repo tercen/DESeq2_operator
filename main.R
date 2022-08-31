e7b974044c80ec28be1876c8ea2d69ec8f0e89bf
agouy <contact@agouy.com>
2022-08-31 14:01:18 +0000

    full refactoring + support for multiple conditions

--- main.R
+++ main.R
@@ -1,5 +1,6 @@
 library(tercen)
-library(tidyverse)
+library(tercenApi)
+library(dplyr)
 library(DESeq2)
 library(ashr)
 library(apeglm)
@@ -12,38 +13,50 @@ if(length(ctx$rnames) == 0) stop("row information is missing.")
 if(length(ctx$cnames) == 0) stop("column information is missing.")
 if(length(ctx$colors) == 0) stop("color information is missing.")
 
-alpha <- as.double(ctx$op.value('alpha'))
-LFC_shrinkage <- as.logical(ctx$op.value('LFC_shrinkage'))
-shrinkage_type <- as.character(ctx$op.value('shrinkage_type'))
+alpha <- ctx$op.value('alpha', as.double, 0.1)
+LFC_shrinkage <- ctx$op.value('LFC_shrinkage', as.logical, TRUE)
+shrinkage_type <- ctx$op.value('shrinkage_type', as.character, "apeglm")
 
+all_data <- ctx$select(c(".ci", ".ri", ".y", ctx$colors[[1]]))
 
-all_data <- ctx %>% select(.ci, .ri,.colorLevels, .y)
-
-count_matrix <- acast(all_data, .ri~.ci, value.var=".y")
+count_matrix <- acast(all_data, .ri ~ .ci, value.var = ".y")
 
 colData <- filter(all_data, .ri == 0) %>%
-  select(.ci, condition = .colorLevels)
+  select(.ci, condition = !!ctx$colors[[1]]) %>%
+  mutate(condition = factor(condition))
 
-colData$condition<-factor(colData$condition)
-
-dds <- DESeqDataSetFromMatrix(countData = count_matrix,
-                              colData = colData,
-                              design = ~ condition)
+dds <- DESeqDataSetFromMatrix(
+  countData = count_matrix,
+  colData = colData,
+  design = ~ condition
+)
 
 dds <- DESeq(dds)
 
-dds_results <- results(dds, alpha = alpha)
+effects <- resultsNames(dds)
+effects <- effects[!effects %in% "Intercept"]
+res <- sapply(effects, function(x) {
+  
+  dds_results <- results(dds, alpha = alpha, name = x)
+  
+  if(LFC_shrinkage) {
+    dds_results <- lfcShrink(
+      dds,
+      type = shrinkage_type,
+      res = dds_results,
+      coef = x
+    )
+  }
+  
+  dds_results$comparison <- x
+  dds_results$.ri <- as.integer(rownames(dds_results))
+  dds_results
+  
+})
 
-if(LFC_shrinkage) dds_results <- lfcShrink(dds, type=shrinkage_type,
-                                           res = dds_results,
-                                           coef = "condition")
-
-res_out_tmp <- tibble(filter(all_data, .ci == 0) %>% select(.ri),
-                      as_tibble(dds_results) %>%
-                        mutate(minus_log10_padj = -log10(padj)))
-
-res_out<-res_out_tmp %>%
+res_out <- do.call(rbind, res) %>%
+  as_tibble() %>%
+  mutate(neg_log10_padj = -log10(padj)) %>%
   ctx$addNamespace()
 
-res_out %>%
-  ctx$save()
+ctx$save(res_out)
